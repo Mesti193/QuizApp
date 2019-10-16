1c0cef0108b0b90a4074f18b4690e5533214b41c
arturostrowski <a.ostrowski193@gmail.com>
2019-10-16 20:02:24 +0200

    Removed unused functions

--- app/src/main/java/com/arturostrowski/quiz/app/data/database/AppDatabase.kt
+++ app/src/main/java/com/arturostrowski/quiz/app/data/database/AppDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.RoomDatabase
 import com.arturostrowski.quiz.app.data.database.repository.user.User
 import com.arturostrowski.quiz.app.data.database.repository.user.UserDao
 
-@Database(entities = [(User::class)], version = 1)
+@Database(entities = [(User::class)], version = 1, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
 
     abstract fun userDao(): UserDao
--- app/src/main/java/com/arturostrowski/quiz/app/ui/splash/interactor/SplashInteractor.kt
+++ app/src/main/java/com/arturostrowski/quiz/app/ui/splash/interactor/SplashInteractor.kt
@@ -3,10 +3,6 @@ package com.arturostrowski.quiz.app.ui.splash.interactor
 import android.content.Context
 import com.google.gson.GsonBuilder
 import com.google.gson.internal.`$Gson$Types`
-import com.arturostrowski.quiz.app.data.database.repository.options.Options
-import com.arturostrowski.quiz.app.data.database.repository.options.OptionsRepo
-import com.arturostrowski.quiz.app.data.database.repository.questions.Question
-import com.arturostrowski.quiz.app.data.database.repository.questions.QuestionRepo
 import com.arturostrowski.quiz.app.data.database.repository.user.UserRepo
 import com.arturostrowski.quiz.app.data.network.ApiHelper
 import com.arturostrowski.quiz.app.data.preferences.PreferenceHelper
@@ -16,28 +12,7 @@ import com.arturostrowski.quiz.app.util.FileUtils
 import io.reactivex.Observable
 import javax.inject.Inject
 
-class SplashInteractor @Inject constructor(private val mContext: Context, private val userRepo: UserRepo, private val questionRepoHelper: QuestionRepo, private val optionsRepoHelper: OptionsRepo, preferenceHelper: PreferenceHelper, apiHelper: ApiHelper) : BaseInteractor(preferenceHelper, apiHelper), SplashMVPInteractor {
-
-    override fun getQuestion(): Observable<List<Question>> {
-        return questionRepoHelper.loadQuestions()
-    }
-
-    override fun seedQuestions(): Observable<Boolean> {
-        val builder = GsonBuilder().excludeFieldsWithoutExposeAnnotation()
-        val gson = builder.create()
-        return questionRepoHelper.isQuestionsRepoEmpty().concatMap { isEmpty ->
-            if (isEmpty) {
-                val type = `$Gson$Types`.newParameterizedTypeWithOwner(null, List::class.java, Question::class.java)
-                val questionList = gson.fromJson<List<Question>>(
-                        FileUtils.loadJSONFromAsset(
-                                mContext,
-                                AppConstants.SEED_DATABASE_QUESTIONS),
-                        type)
-                questionRepoHelper.insertQuestions(questionList)
-            } else
-                Observable.just(false)
-        }
-    }
+class SplashInteractor @Inject constructor(private val mContext: Context, private val userRepo: UserRepo, preferenceHelper: PreferenceHelper, apiHelper: ApiHelper) : BaseInteractor(preferenceHelper, apiHelper), SplashMVPInteractor {
 
     override fun isUserLogged(): Observable<Boolean> {
         return userRepo.isUserRepoEmpty().concatMap { isEmpty ->
@@ -50,20 +25,4 @@ class SplashInteractor @Inject constructor(private val mContext: Context, privat
 
     }
 
-    override fun seedOptions(): Observable<Boolean> {
-        val builder = GsonBuilder().excludeFieldsWithoutExposeAnnotation()
-        val gson = builder.create()
-        return optionsRepoHelper.isOptionsRepoEmpty().concatMap { isEmpty ->
-            if (isEmpty) {
-                val type = `$Gson$Types`.newParameterizedTypeWithOwner(null, List::class.java, Options::class.java)
-                val optionsList = gson.fromJson<List<Options>>(
-                        FileUtils.loadJSONFromAsset(
-                                mContext,
-                                AppConstants.SEED_DATABASE_OPTIONS),
-                        type)
-                optionsRepoHelper.insertOptions(optionsList)
-            } else
-                Observable.just(false)
-        }
-    }
 }
